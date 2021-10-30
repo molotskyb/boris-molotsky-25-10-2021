6dfdd941c4c78d27c0e58c09fc167d0af93d9f67
molotskyb <molotskyb@gmail.com>
2021-10-30 15:44:23 +0300

    update

--- src/store/reducers/acuuWeatherApiReducers.js
+++ src/store/reducers/acuuWeatherApiReducers.js
@@ -1,65 +1,63 @@
-
-
 export const autocompleteSearchListReducer = (
-  state = { locations: [] },
-  action
+	state = { locations: [] },
+	action
 ) => {
-  switch (action.type) {
-    case 'AUTOCOMPLETE_SEARCH_LIST_SUCCESS':
-      return {
-        locations: action.payload,
-      }
-    case 'AUTOCOMPLETE_SEARCH_LIST_FAIL':
-      return { error: action.payload }
-    default:
-      return state
-  }
-}
+	switch (action.type) {
+		case "AUTOCOMPLETE_SEARCH_LIST_SUCCESS":
+			return {
+				locations: action.payload,
+			};
+		case "AUTOCOMPLETE_SEARCH_LIST_FAIL":
+			return { error: action.payload };
+		default:
+			return state;
+	}
+};
 
 export const autocompleteSearchLocationReducer = (
-  state = { location: [] },
-  action
+	state = { location: [] },
+	action
 ) => {
-  switch (action.type) {
-    case 'AUTOCOMPLETE_SEARCH_LOCATION_SUCCESS':
-      return {
-        location: action.payload,
-      }
-    case 'AUTOCOMPLETE_SEARCH_LOCATION_FAIL':
-      return { error: action.payload }
-    default:
-      return state
-  }
-}
+	switch (action.type) {
+		case "AUTOCOMPLETE_SEARCH_LOCATION_SUCCESS":
+			return {
+				location: action.payload,
+			};
+		case "AUTOCOMPLETE_SEARCH_LOCATION_FAIL":
+			return { error: action.payload };
+		default:
+			return state;
+	}
+};
 
 export const currentConditionReducer = (
-  state = { currentCondition: [] },
-  action
+	state = { currentCondition: [] },
+	action
 ) => {
-  switch (action.type) {
-    case 'CURRENT_CONDITION_SUCCESS':
-      return {
-        currentCondition: action.payload,
-      }
-    case 'CURRENT_CONDITION_FAIL':
-      return { error: action.payload }
-    default:
-      return state
-  }
-}
+	switch (action.type) {
+		case "CURRENT_CONDITION_SUCCESS":
+			return {
+				currentCondition: action.payload,
+			};
+		case "CURRENT_CONDITION_FAIL":
+			return { error: action.payload };
+		default:
+			return state;
+	}
+};
 
 export const fiveDaysConditionReducer = (
-  state = { fiveDaysCondition: {} },
-  action
+	state = { fiveDaysCondition: {} },
+	action
 ) => {
-  switch (action.type) {
-    case 'FIVE_DAYS_CONDITION_SUCCESS':
-      return {
-        fiveDaysCondition: action.payload,
-      }
-    case 'FIVE_DAYS_CONDITION_FAIL':
-      return { error: action.payload }
-    default:
-      return state
-  }
-}
+	switch (action.type) {
+		case "FIVE_DAYS_CONDITION_SUCCESS":
+			return {
+				fiveDaysCondition: action.payload,
+			};
+		case "FIVE_DAYS_CONDITION_FAIL":
+			return { error: action.payload };
+		default:
+			return state;
+	}
+};
