1cb58c004da789f0be7bac73e3c0f6e90a894a0d
molotskyb <molotskyb@gmail.com>
2021-10-31 05:43:51 +0200

    config update

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
-export const myApiKey = "ukNHOdRFi4CB4dOh7TfzRzZRGIipgphI";
-// export const myApiKey = "GElJx8PRyAGJvlUmeGdl75y6G6aULRsm";
+// export const myApiKey = "ukNHOdRFi4CB4dOh7TfzRzZRGIipgphI";
+export const myApiKey = "dvQQtNqeVlNvH6UvCOXO4iQaJkKM4ZwF";
